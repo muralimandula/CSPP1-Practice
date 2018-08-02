b70fe40f95cf16d7b7ec08933a3208c84cb00069
muralimandula <murali.mandula@msitprogram.net>
2018-08-01 22:12:28 -0700

    activity 12 done

---
+++ m3/m13_a12_u3.py
@@ -0,0 +1,9 @@
+"""m13_a12_u3"""
+NUM = 10
+while True:
+    if NUM < 7:
+        print('Breaking out of loop')
+        break
+    print(NUM)
+    NUM -= 1
+print('Outside of loop')
---
+++ m3/m13_a12_u4.py
@@ -0,0 +1,6 @@
+"""m13_a12_u4"""
+NUM = 100
+while not False:
+    if NUM < 0:
+        break
+print('NUM is: ' + str(NUM))
